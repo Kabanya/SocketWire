a9d795e324ae8bb40b89ff60a4e105c9bc0b0527
Kabanya <evonirazh@mail.ru>
2025-11-04 19:43:54 +0300

    licence and update thirdparty

--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -1,14 +1,5 @@
 cmake_minimum_required(VERSION 3.28)
 
-# Simple logging without headaches
-CPMAddPackage(
-  NAME spdlog
-  GITHUB_REPOSITORY gabime/spdlog
-  VERSION 1.15.3
-  OPTIONS
-    SPDLOG_USE_STD_FORMAT ON  # std::format from C++20
-)
-
 # Google Test for unit testing
 CPMAddPackage(
   NAME googletest
